e103be553267aba1cd13a7917d8daa9bcec006d0
chiuko <chiuko@ifalodeMac-mini-7.local>
2016-05-05 17:56:19 +0800

    增加語系包

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -3,12 +3,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
 
+    private $language;
+
+    /**
+     * setting
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->language = $this->session->language;
+
+        if (isset($this->language)) {
+            //設定語系
+            $this->lang->load($this->config->item('language_file_name'), $this->session->language);
+        }
+    }
+
 	/**
 	 * 後台資料列表
 	 */
 	public function dataList()
 	{
-		$data = array('content' => $this->load->view('admin/list', '', true));
+		$data = array(
+            'content' => $this->load->view('admin/list', '', true),
+            'layout'  => $this->lang->line('layout')
+        );
+
 		$this->load->view('admin/layout', $data);
 	}
 }
---
+++ application/language/chinese/all_lang.php
@@ -0,0 +1,6 @@
+<?php
+$lang['layout'] = array(
+    'top_setting' => '設定',
+    'top_logout' => '登出',
+    'left_menu' => '首頁',
+);
