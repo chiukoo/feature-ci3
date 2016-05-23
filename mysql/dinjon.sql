a62997db22b9a9fc37ed4ca205ecf92b727d1717
chiuko <chiuko@ifalo.com.tw>
2016-05-23 20:57:12 +0800

    add sql

--- mysql/dinjon.sql
+++ mysql/dinjon.sql
@@ -1,135 +1,140 @@
--- phpMyAdmin SQL Dump
--- version 4.4.14
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.7.9, for osx10.9 (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: May 22, 2016 at 06:11 AM
--- Server version: 5.6.26
--- PHP Version: 5.6.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: 10.10.10.10    Database: dinjon
+-- ------------------------------------------------------
+-- Server version 5.5.49-MariaDB-1ubuntu0.14.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `dinjon`
---
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `account`
 --
 
-CREATE TABLE IF NOT EXISTS `account` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `password` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `create_dt` datetime DEFAULT NULL,
-  `order` int(11) DEFAULT '0'
+  `order` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `account`
 --
 
-INSERT INTO `account` (`id`, `username`, `password`, `create_dt`, `order`) VALUES
-(6, 'admin', '$2y$10$EFefNlSXHJqD8.Hv78KSv.uJsoaxJuKs3iGIP/h9FYCQvAYq/Mpe2', '2016-05-08 18:46:28', 19),
-(21, '123', '$2y$10$K03nE9xeFso8F9iPZMf//O0pHAltvcy7K7BA6s0y2YfNvH28jHjxu', '2016-05-17 23:56:36', 18),
-(24, '2', '$2y$10$wKs10GhgUjmQGa7WbH6CueyJvHsFa8742pKWVbWM2iOkjFKI5qACa', '2016-05-18 01:03:58', 7);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `product_project`
---
-
-CREATE TABLE IF NOT EXISTS `product_project` (
-  `id` int(11) NOT NULL,
-  `title` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `lang` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `order` int(10) NOT NULL DEFAULT '0',
-  `create_dt` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+INSERT INTO `account` VALUES (6,'admin','$2y$10$EFefNlSXHJqD8.Hv78KSv.uJsoaxJuKs3iGIP/h9FYCQvAYq/Mpe2','2016-05-08 18:46:28',19),(21,'123','$2y$10$K03nE9xeFso8F9iPZMf//O0pHAltvcy7K7BA6s0y2YfNvH28jHjxu','2016-05-17 23:56:36',18),(24,'2','$2y$10$wKs10GhgUjmQGa7WbH6CueyJvHsFa8742pKWVbWM2iOkjFKI5qACa','2016-05-18 01:03:58',7);
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Dumping data for table `product_project`
+-- Table structure for table `product_inner`
 --
 
-INSERT INTO `product_project` (`id`, `title`, `lang`, `order`, `create_dt`) VALUES
-(1, 'fffddd', 'ch', 2, '2016-05-21 15:59:54'),
-(3, 'RFR', 'ch', 1, '2016-05-21 16:19:09');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `product_type`
---
-
-CREATE TABLE IF NOT EXISTS `product_type` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `product_inner`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_inner` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `lang` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   `create_dt` datetime DEFAULT NULL,
   `order` int(10) DEFAULT '0',
   `project` int(5) NOT NULL,
-  `img_url` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `type` int(5) NOT NULL,
+  `img_url` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_inner`
+--
+
+LOCK TABLES `product_inner` WRITE;
+/*!40000 ALTER TABLE `product_inner` DISABLE KEYS */;
+INSERT INTO `product_inner` VALUES (6,'aa','ch','2016-05-22 21:08:16',1,3,5,'/feature-ci3/assets/filemanager/userfiles/img/2.JPG'),(14,'123','ch','2016-05-22 22:13:37',2,3,5,'');
+/*!40000 ALTER TABLE `product_inner` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_project`
+--
+
+DROP TABLE IF EXISTS `product_project`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lang` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `order` int(10) NOT NULL DEFAULT '0',
+  `create_dt` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_project`
+--
+
+LOCK TABLES `product_project` WRITE;
+/*!40000 ALTER TABLE `product_project` DISABLE KEYS */;
+INSERT INTO `product_project` VALUES (3,'RFR','ch',1,'2016-05-21 16:19:09');
+/*!40000 ALTER TABLE `product_project` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_type`
+--
+
+DROP TABLE IF EXISTS `product_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lang` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `create_dt` datetime DEFAULT NULL,
+  `order` int(10) DEFAULT '0',
+  `project` int(5) NOT NULL,
+  `img_url` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `product_type`
 --
 
-INSERT INTO `product_type` (`id`, `title`, `lang`, `create_dt`, `order`, `project`, `img_url`) VALUES
-(4, '項目一', 'ch', '2016-05-21 21:50:34', 5, 3, ''),
-(5, 'dddsada', 'ch', '2016-05-21 21:51:17', 4, 3, '/ci-admin-ck-file/assets/filemanager/userfiles/img/82102.jpg');
+LOCK TABLES `product_type` WRITE;
+/*!40000 ALTER TABLE `product_type` DISABLE KEYS */;
+INSERT INTO `product_type` VALUES (5,'dddsada','ch','2016-05-21 21:51:17',4,3,'/feature-ci3/assets/filemanager/userfiles/img/2.JPG'),(11,'aaazz','ch','2016-05-22 22:15:03',5,3,'');
+/*!40000 ALTER TABLE `product_type` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `account`
---
-ALTER TABLE `account`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `product_project`
---
-ALTER TABLE `product_project`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `product_type`
---
-ALTER TABLE `product_type`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `account`
---
-ALTER TABLE `account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=25;
---
--- AUTO_INCREMENT for table `product_project`
---
-ALTER TABLE `product_project`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `product_type`
---
-ALTER TABLE `product_type`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-05-23 20:54:23
